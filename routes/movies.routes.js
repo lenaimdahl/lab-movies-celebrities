f9d7b1b42462f93318ba820ae4e551a975e6cc60
Lena Imdahl <mail@lenaimdahl.de>
2023-04-09 12:01:17 +0200

    Iteration 6

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,7 +1,18 @@
 const router = require("express").Router();
 
-router.get("/movie", (req, res) => {
+router.get("/edit-movie", (req, res) => {
   res.render("movies/edit-movie");
+});
+
+router.post("/edit-movie", async (req, res) => {
+  try {
+    const newMovie = await MovieModel.create(req.body);
+    console.log("New Movie Created ", newMovie);
+    res.redirect("movies");
+  } catch (err) {
+    console.log("there was an error", err);
+    res.redirect("movies/edit-movie");
+  }
 });
 
 module.exports = router;
