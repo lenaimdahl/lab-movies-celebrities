0a37bbc0b9f0c6b2d8b9dcaa59b09b6c4edcb55c
Lena Imdahl <mail@lenaimdahl.de>
2023-04-09 12:06:17 +0200

    changed file names and routes

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 const CelebrityModel = require("../models/Celebrity.model");
 
-router.get("/create", (req, res) => {
+router.get("/create-celebrity", (req, res) => {
   res.render("celebrities/new-celebrity");
 });
 
-router.post("/create", async (req, res) => {
+router.post("/create-celebrity", async (req, res) => {
   try {
     const newCelebrity = await CelebrityModel.create(req.body);
     console.log("New celebrity Created ", newCelebrity);
